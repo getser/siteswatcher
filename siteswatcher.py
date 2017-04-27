c2e023207acbbb3199653d608559422b4e497b77
Sergii Getmanchuk <gsforwork@gmail.com>
2017-04-27 19:33:30 +0300

    add json load, mail sending

--- siteswatcher.py
+++ siteswatcher.py
@@ -1,7 +1,49 @@
+import json
 import urllib.request
+import smtplib
 
 
-urls = ['http://docs.python.org/', 'https://www.google.com/1']
+# urls = ['http://docs.python.org/', 'https://www.google.com/1']
+
+def load_from_json(jsonfile):
+    """
+    urlssfile - string() filename
+
+    Returns:
+        dict() settings loaded from file.
+    """
+
+    try:
+        with open(jsonfile) as loaded_file:
+            loaded = json.loads(loaded_file.read())
+    except IOError:
+        print("The data is not loaded! Please, check if file {} is present in working directory.".format(jsonfile))
+        raise IOError
+    return loaded
+
+
+def send_email(settings, msg):
+
+    # If using gmail, we need to unlock
+    # Captcha to anable script to send
+    # for you:
+    # https://accounts.google.com/displayunlockcaptcha
+    message = 'Subject: URLs problems report\n\n' + msg
+    toaddrs = settings["RECIPIENTS_ADDRESS"]  #['user1_to@gmail.com',]
+    # msg = settings#'Why,Oh why!'
+    username = fromaddr = settings["EMAIL_HOST_USER"]  #'user_from@gmail.com'
+    password = settings["EMAIL_HOST_PASSWORD"]  #'pwd'
+    server = smtplib.SMTP(':'.join([settings["EMAIL_HOST"], str(settings["EMAIL_PORT"])]))
+    server.ehlo()
+    server.starttls()
+    server.login(username, password)
+    server.sendmail(fromaddr, toaddrs, message)
+    server.quit()
+
+
+urls = load_from_json('sitesurls.json')
+email_settings = load_from_json('mail_settings.json')
+message = ''
 
 for url in urls:
     try:
@@ -11,9 +53,19 @@ for url in urls:
         else:
             print('The response code was not 200, but: {}'.format(
                 response.get_code()))
+
+            message += '\nThe site at URL {} is probably broken. The response code was not 200, but: {}.\n'.format(
+                url, response.get_code())
+
     except urllib.error.HTTPError as e:
-        print('''An error occurred: {}
-    The response code was {}'''.format(e, e.getcode()))
+        print('The site at URL {} is probably broken. An error occurred: {}. The response code was {}.'.format(
+            url, e, e.getcode()))
+
+        message += '\nThe site at URL {} is probably broken. An error occurred: {}. The response code was {}.\n'.format(
+            url, e, e.getcode())
 
     except ValueError:
-        print('''Wrong URL: {}'''.format(url))
+        print('Wrong URL: {}'.format(url))
+
+if message:
+    send_email(email_settings, message)
